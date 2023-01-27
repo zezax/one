4c7828ce490dd751d809d7a95062dbfd96d8fc17
Alex <github@zezax.com>
2023-01-27 10:52:57 -0800

    Reduced repeated code in Proxy.h (DRY)

--- quol/red/include/Proxy.h
+++ quol/red/include/Proxy.h
@@ -49,166 +49,55 @@ private:
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// FIXME: make State * state_ member, next() method
-
 template <Format FMT>
-class DfaProxy {};
+struct DfaDefs {};
 
 template <>
-class DfaProxy<fmtOffset1> {
-public:
+struct DfaDefs<fmtOffset1> {
   typedef StateOffset1 State;
-  typedef uint8_t Value;
+  typedef uint8_t      Value;
 
   static constexpr Value  resultMask_   = 0x7f;
   static constexpr Result maxResult_    = 0x7f;
   static constexpr size_t maxOffset_    = 0xff;
   static constexpr int    deadEndShift_ = 7;
-
-  static bool resultFits(Result maxResult) {
-    return (maxResult <= maxResult_);
-  }
-
-  static bool offsetFits(CharIdx maxChar, size_t numStates) {
-    // !!! assume result is same size as transition entries
-    size_t tot = (numStates * maxChar) + numStates + numStates;
-    return (tot <= maxOffset_);
-  }
-
-  static void checkCapacity(size_t  numStates,
-                            CharIdx maxChar,
-                            Result  maxResult) {
-    if (!resultFits(maxResult) || !offsetFits(maxChar, numStates))
-      throw RedExceptLimit("DFA too big for 1-byte format");
-  }
-
-  static size_t stateSize(CharIdx maxChar) {
-    return sizeof(State) + (sizeof(Value) * maxChar) + sizeof(Value);
-  }
-
-  static void appendOff(std::string &buf, size_t off) {
-    if (off > maxOffset_)
-      throw RedExceptSerialize("overflow in 1-byte appendOff");
-    Value raw = static_cast<Value>(off);
-    buf.append(reinterpret_cast<const char *>(&raw), sizeof(raw));
-  }
-
-  static uint8_t resultAndDeadEnd(Result res, bool de) {
-    return (res & resultMask_) | (de << deadEndShift_);
-  }
-
-  Result result() const {
-    return state_->resultAndDeadEnd_ & resultMask_;
-  }
-
-  bool deadEnd() const {
-    return state_->resultAndDeadEnd_ >> deadEndShift_;
-  }
-
-  bool pureDeadEnd() const {
-    return (state_->resultAndDeadEnd_ == (1u << deadEndShift_));
-  }
-
-  void init(const char *base, size_t offset) {
-    state_ = reinterpret_cast<const State *>(base + offset);
-  }
-
-  size_t trans(size_t byte) const {
-    return state_->offsets_[byte] * sizeof(Value);
-  }
-
-  void next(const char *base, size_t byte) { init(base, trans(byte)); }
-
-  const State *state() const { return state_; }
-
-private:
-  const State *__restrict__ state_;
 };
 
 
 template <>
-class DfaProxy<fmtOffset2> {
-public:
+struct DfaDefs<fmtOffset2> {
   typedef StateOffset2 State;
-  typedef uint16_t Value;
+  typedef uint16_t     Value;
 
   static constexpr Value  resultMask_   = 0x7fff;
   static constexpr Result maxResult_    = 0x7fff;
   static constexpr size_t maxOffset_    = 0xffff;
   static constexpr int    deadEndShift_ = 15;
-
-  static bool resultFits(Result maxResult) {
-    return (maxResult <= maxResult_);
-  }
-
-  static bool offsetFits(CharIdx maxChar, size_t numStates) {
-    // !!! assume result is same size as transition entries
-    size_t tot = (numStates * maxChar) + numStates + numStates;
-    return (tot <= maxOffset_);
-  }
-
-  static void checkCapacity(size_t  numStates,
-                            CharIdx maxChar,
-                            Result  maxResult) {
-    if (!resultFits(maxResult) || !offsetFits(maxChar, numStates))
-      throw RedExceptLimit("DFA too big for 2-byte format");
-  }
-
-  static size_t stateSize(CharIdx maxChar) {
-    return sizeof(State) + (sizeof(Value) * maxChar) + sizeof(Value);
-  }
-
-  static void appendOff(std::string &buf, size_t off) {
-    off /= sizeof(Value);
-    if (off > maxOffset_)
-      throw RedExceptSerialize("overflow in 2-byte appendOff");
-    Value raw = static_cast<Value>(off);
-    buf.append(reinterpret_cast<const char *>(&raw), sizeof(raw));
-  }
-
-  static uint16_t resultAndDeadEnd(Result res, bool de) {
-    return (res & resultMask_) | (de << deadEndShift_);
-  }
-
-  Result result() const {
-    return state_->resultAndDeadEnd_ & resultMask_;
-  }
-
-  bool deadEnd() const {
-    return state_->resultAndDeadEnd_ >> deadEndShift_;
-  }
-
-  bool pureDeadEnd() const {
-    return (state_->resultAndDeadEnd_ == (1u << deadEndShift_));
-  }
-
-  void init(const char *base, size_t offset) {
-    state_ = reinterpret_cast<const State *>(base + offset);
-  }
-
-  size_t trans(size_t byte) const {
-    return state_->offsets_[byte] * sizeof(Value);
-  }
-
-  void next(const char *base, size_t byte) { init(base, trans(byte)); }
-
-  const State *state() const { return state_; }
-
-private:
-  const State *__restrict__ state_;
 };
 
 
 template <>
-class DfaProxy<fmtOffset4> {
-public:
+struct DfaDefs<fmtOffset4> {
   typedef StateOffset4 State;
-  typedef uint32_t Value;
+  typedef uint32_t     Value;
 
   static constexpr Value  resultMask_   = 0x7fffffff;
   static constexpr Result maxResult_    = 0x7fffffff;
   static constexpr size_t maxOffset_    = 0xffffffff;
   static constexpr int    deadEndShift_ = 31;
+};
+
+
+template <Format FMT>
+class DfaProxy {
+public:
+  typedef typename DfaDefs<FMT>::State State;
+  typedef typename DfaDefs<FMT>::Value Value;
+
+  static constexpr Value  resultMask_   = DfaDefs<FMT>::resultMask_;
+  static constexpr Result maxResult_    = DfaDefs<FMT>::maxResult_;
+  static constexpr size_t maxOffset_    = DfaDefs<FMT>::maxOffset_;
+  static constexpr int    deadEndShift_ = DfaDefs<FMT>::deadEndShift_;
 
   static bool resultFits(Result maxResult) {
     return (maxResult <= maxResult_);
@@ -224,7 +113,7 @@ public:
                             CharIdx maxChar,
                             Result  maxResult) {
     if (!resultFits(maxResult) || !offsetFits(maxChar, numStates))
-      throw RedExceptLimit("DFA too big for 4-byte format");
+      throw RedExceptLimit("DFA too big for format");
   }
 
   static size_t stateSize(CharIdx maxChar) {
@@ -234,12 +123,12 @@ public:
   static void appendOff(std::string &buf, size_t off) {
     off /= sizeof(Value);
     if (off > maxOffset_)
-      throw RedExceptSerialize("overflow in 4-byte appendOff");
+      throw RedExceptSerialize("overflow in appendOff");
     Value raw = static_cast<Value>(off);
     buf.append(reinterpret_cast<const char *>(&raw), sizeof(raw));
   }
 
-  static uint32_t resultAndDeadEnd(Result res, bool de) {
+  static Value resultAndDeadEnd(Result res, bool de) {
     return (res & resultMask_) | (de << deadEndShift_);
   }
 
